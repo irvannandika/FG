bd4f4fd9cac458858ffa0819ab64d9cc31cb5738
irvannandika <54287308+irvannandika@users.noreply.github.com>
2019-08-20 03:08:56 +0800

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,21 @@
+<?php
+/* setting bot */
+$ads=$ijo."    [ Auto Claim By Irvannandika ]".$t;
+$donasi=false;
+
+$figlet=$ijo." IrvanNandika ".$t;
+$warn=$red."[!] Warning !!!
+".$kn."[-] Saya tidak bertanggung jawab
+".$kn."[-] resiko sepenuhnya di tanggung pengguna!!
+".$kn."[-] ini adalah program ilegal".$t;
+
+$flash="$red
+ █▀──── █────── ▄▀▄──── ▄▀▀──── █────── ▄▀▀───── ▄▀▄
+ █▀──── █─▄──── █▀█──── ─▀▄──── █▀▄──── █─▀▌──── █─█
+ ▀───── ▀▀▀──── ▀─▀──── ▀▀───── ▀─▀──── ▀▀▀───── ─▀─
+$putih  Mod By :$ijo IrvanNandika$putih ||||| FB :$turkis IrvanNandika".$t."$putih  Support :$ijo VTeam".$t;
+$msg_flash=$turkis."FlashGo";
+$stat_flash=true;
+
+
+?>
---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+$stat_sseth=true;
+$msgsseth= $figlet."IrvanNandika";
+$stat_cp = true;
+$msgcp = "Bot Claim by IrvanNandika";
+$figlet= $ijo."
+───────────────────────────────────────────
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
+── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
+── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
+  $putih."Mod By:".$ijo." IrvanNandika   ".$t;
+  
+?>
